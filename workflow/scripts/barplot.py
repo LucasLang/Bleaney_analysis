367564ad1f11f1a046a14df6367c01572229e0cc
Lucas Lang <lucas.lang@chem.tu-berlin.de>
2025-12-11 10:25:06 +0100

    Now both plotting scripts load a common plotting function from a third file.

---
+++ workflow/scripts/barplot.py
@@ -0,0 +1,27 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+def barplot_chi(rowlabels, columnlabels, data, ylabel, outfile):
+    x = np.arange(len(rowlabels))  # this is a dummy number that is not displayed in the plot
+    width = 0.18  # width of each bar
+
+    fig, ax = plt.subplots(figsize=(5, 3))
+
+    # Colors for each method
+    colors = ["#1f77b4", "#ff7f0e", "#2ca02c", "#d62728"]
+
+    # Plot each method's bars
+    for i, (method, color) in enumerate(zip(columnlabels, colors)):
+        ax.bar(x + i * width - 1.5 * width, data[:, i], width, label=method, color=color, edgecolor='black', linewidth=0.8)
+
+    # Axis and labels
+    ax.axhline(0, color='black', linewidth=0.9)
+    ax.set_xticks(x)
+    ax.set_xticklabels(rowlabels)
+    ax.set_ylabel(ylabel)
+    ax.legend(frameon=False)
+    ax.set_xlim(x[0] - 0.5, x[-1] + 0.5)
+    ax.tick_params(direction='in', top=True, right=True)
+
+    plt.tight_layout()
+    plt.savefig(outfile, dpi=300)
--- workflow/scripts/barplot_chiax.py
+++ workflow/scripts/barplot_chiax.py
@@ -1,6 +1,6 @@
 import numpy as np
-import matplotlib.pyplot as plt
 import sys
+from barplot import barplot_chi
 
 Ln_list_file = sys.argv[1]
 chi_ax_file = sys.argv[2]
@@ -15,29 +15,7 @@ values_CASSCF = np.loadtxt(chi_ax_CASSCF_file)
 values_CASSCF = np.atleast_2d(values_CASSCF).T
 values = np.hstack((values_calculated, values_CASSCF))
 
-methods = ["Bleaney", "3rd order", "exact", "CASSCF"]
+methods = ["Bleaney (2nd order)", "3rd order", "Exact dyadic", "CASSCF"]
+ylabel = r"$\chi_\mathrm{ax}$ / $\AA^3$"
+barplot_chi(Ln_list, methods, values, ylabel, outfile)
 
-x = np.arange(len(Ln_list))  # this is a dummy number that is not displayed in the plot
-width = 0.18  # width of each bar
-
-fig, ax = plt.subplots(figsize=(8, 5))
-
-# Colors for each method
-colors = ["#1f77b4", "#ff7f0e", "#2ca02c", "#d62728"]
-
-# Plot each method's bars
-for i, (method, color) in enumerate(zip(methods, colors)):
-    ax.bar(x + i * width - 1.5 * width, values[:, i], width, label=method, color=color, edgecolor='black', linewidth=0.8)
-
-# Axis and labels
-ax.axhline(0, color='black', linewidth=0.9)
-ax.set_xticks(x)
-ax.set_xticklabels(Ln_list)
-ax.set_ylabel("Calculated Property (units)")
-ax.set_xlabel("Molecule")
-ax.legend(frameon=False)
-ax.set_xlim(x[0] - 0.5, x[-1] + 0.5)
-ax.tick_params(direction='in', top=True, right=True)
-
-plt.tight_layout()
-plt.savefig(outfile, dpi=300)
